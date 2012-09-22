66248dc3985f5189430d3a98f11cac05537dce13
David Silver <dsilver@candidatemetrics.com>
2012-09-22 17:10:27 -0400

    setting up

--- blog-after/config/routes.rb
+++
@@ -1,7 +0,0 @@
-Blog::Application.routes.draw do
-  resources :articles
-  root to: 'articles#index'
-  match 'auth/:provider/callback', to: 'sessions#create'
-  match 'auth/failure', to: redirect('/')
-  match 'signout', to: 'sessions#destroy', as: 'signout'
-end
---
+++ blog/config/initializers/omniauth.rb
@@ -0,0 +1,3 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+	provider :twitter, 'N0Zut2yr9koNshhLHkPQ', 'zpFE8FrrWVt9c4NqtcjuPtX6NWlcf6PaKdSYdp3w'
+end
