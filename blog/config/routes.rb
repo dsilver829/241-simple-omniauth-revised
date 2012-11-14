661af3ef369165d3a82a6491f290fae6105db09a
David Silver <dsilver@candidatemetrics.com>
2012-11-13 20:33:57 -0800

    add sign out capability

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -2,4 +2,5 @@ Blog::Application.routes.draw do
   resources :articles
   root to: 'articles#index'
   match 'auth/:provider/callback', to: 'sessions#create'
+  match 'signout', to: 'sessions#destroy', as: 'signout'
 end
